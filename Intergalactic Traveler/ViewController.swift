3ccd08c71a7fbbaf0fa0c7889fd4ea323f3dcb19
megan <mkleve1172@stu.d214.org>
2019-11-06 11:33:24 -0600

    Add all files.

---
+++ Intergalactic Traveler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Intergalactic Traveler
+//
+//  Created by Megan Kleve on 11/6/19.
+//  Copyright © 2019 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
